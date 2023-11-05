6c9f040f9d99f97666a2d631dd513548f7384431
Richik Gangopadhyay <123389121+RH0IS@users.noreply.github.com>
2023-11-05 01:29:58 -0500

    richik | main | trends page setup

--- coinmarketapp/urls.py
+++ coinmarketapp/urls.py
@@ -4,8 +4,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.demo, name='crypto_list'),
-    path('home',views.crypto_list, name='crypto_list' ),
-path('trends/', views.trends_view, name='trends_view'),
+    path('', views.demo, name='demo'),
+    path('demo', views.demo, name='demo'),
+    path('home/',views.crypto_list, name='crypto_list' ),
+    path('trends/', views.trends_view, name='trends_view'),
     path('error',views.error, name='error' ),
 ]
