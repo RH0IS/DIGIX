1cde53622a720411026a58f203d9a16c559d19d5
Ruchika <ruchika.3brain@gmail.com>
2023-11-14 01:19:56 +0530

    Ruchika Gohil | authentication | working on the profile pic upload

---
+++ authentication/forms.py
@@ -0,0 +1,12 @@
+# userauth/forms.py
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Enter a valid email address.')
+    profile_picture = forms.ImageField(required=False, label='Profile Picture')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', 'profile_picture')
