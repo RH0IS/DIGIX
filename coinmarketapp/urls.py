98c760720af032fdf4ebd06702ba7ce7c3299324
whichFahim <31901239+whichfahim@users.noreply.github.com>
2023-11-15 02:24:14 -0600

    Fahim | highlights | polished the highlights section, changed the trends page to the home page, and added local images for cryptocurrency logos

--- coinmarketapp/urls.py
+++ coinmarketapp/urls.py
@@ -4,13 +4,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.demo, name='demo'),
-    path('demo', views.demo, name='demo'),
-    path('home/', views.crypto_list, name='crypto_list'),
-    path('trends/', views.trends_view, name='trends_view'),
-    path('user_profile/', views.user_profile, name='user_profile'),
-    path('error', views.error, name='error'),
-    path('checkout', views.payment_test, name='payment_test_page'),
-    path('create-payment-intent', views.create_payment, name='create-payment-intent'),
-    path('payment-result', views.payment_result, name='payment-result'),
+    path("", views.demo, name="demo"),
+    path("demo", views.demo, name="demo"),
+    # path('home/', views.crypto_list, name='crypto_list'),
+    path("home/", views.trends_view, name="trends_view"),
+    path("trends/", views.trends_view, name="trends_view"),
+    path("user_profile/", views.user_profile, name="user_profile"),
+    path("error", views.error, name="error"),
+    path("checkout", views.payment_test, name="payment_test_page"),
+    path("create-payment-intent", views.create_payment, name="create-payment-intent"),
+    path("payment-result", views.payment_result, name="payment-result"),
 ]
