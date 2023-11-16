b5ca097d520282431433478c7c2edeca314a523c
whichFahim <31901239+whichfahim@users.noreply.github.com>
2023-11-15 23:38:42 -0600

    Revert "Ruchika Gohil | Main | Worked on the Change Password functionality"
    This reverts commit 9a95d494288406712ff45beadf0e7de413834563.

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
         path('login/', views.login_view, name='login'),
     path('logout/', views.custom_logout, name='logout'),        
-        path('change_password/', views.change_password, name='change_password'),
 
     path('user_profile/', views.user_profile, name='user_profile'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -9,10 +9,8 @@ from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.decorators import login_required
 
 
-
-
 def signup(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         print(f"Received a POST request to the 'signup' view")
 
         form = CustomUserCreationForm(request.POST)
@@ -23,54 +21,56 @@ def signup(request):
             profile.user = user
             profile.save()
             print(user)
-            return redirect('login')
+            return redirect("login")
     else:
         form = CustomUserCreationForm()
         profile_form = UserProfileForm()
-    return render(request, 'authentication/signup.html', {'form': form})
+    return render(request, "authentication/signup.html", {"form": form})
+
 
 def user_profile(request):
     if request.user.is_authenticated:
         try:
             profile = UserProfile.objects.get(user=request.user)
             if profile.registered:
-                return render(request, 'authentication/login.html')
+                return render(request, "authentication/login.html")
         except UserProfile.DoesNotExist:
             pass
-    return render(request, 'authentication/registered_user.html')
+    return render(request, "authentication/registered_user.html")
+
 
 def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('crypto_list')
+            return redirect("crypto_list")
         else:
             error_message = "Invalid username or password. Please try again!"
             messages.error(request, error_message)
-            return render(request, 'authentication/login.html')
-    return render(request, 'authentication/login.html')
+            return render(request, "authentication/login.html")
+    return render(request, "authentication/login.html")
+
 
 def custom_logout(request):
-        logout(request)
+    logout(request)
 
-        return redirect('login')
-
+    return redirect("login")
 
 
 @login_required
 def change_password(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = PasswordChangeForm(request.user, request.POST)
         if form.is_valid():
             user = form.save()
-            update_session_auth_hash(request, user)  
-            messages.success(request, 'Your password was successfully updated!')
-            return redirect('login')
+            update_session_auth_hash(request, user)
+            messages.success(request, "Your password was successfully updated!")
+            return redirect("login")
         else:
-            messages.error(request, 'Please correct the error below.')
+            messages.error(request, "Please correct the error below.")
     else:
         form = PasswordChangeForm(request.user)
-    return render(request, 'authentication/change_password.html', {'form': form})
+    return render(request, "authentication/change_password.html", {"form": form})
