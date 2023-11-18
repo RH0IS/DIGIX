2bd21f40061c827689ddf04bb340366c6ce1540c
chandr15 <chandr15@uwindsor.ca>
2023-11-18 15:45:50 -0500

    Aditi | converter | updated converter

--- Digix/settings.py
+++ Digix/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
+import os
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-@-c%&!phm#lt9(ld7!&ycq_ls0qivs6t@m%j^_=##rnz)79z%j"
 
@@ -126,6 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = []
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
