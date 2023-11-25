c8bce66b439f31e0e8a4744e1867ef32a77b8d29
Ruchika <ruchika.3brain@gmail.com>
2023-11-25 20:55:52 +0530

    Modifications in user profile

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('signup/', views.signup, name='signup'),
+    path('signup/', views.signup, name='signup'),
         path('login/', views.login_view, name='login'),
-        path('logout/', views.custom_logout, name='logout'),        
+    path('logout/', views.custom_logout, name='logout'),        
         path('change_password/', views.change_password, name='change_password'),
-        path('user_profile/', views.user_profile, name='user_profile'),
+
+    path('user_profile/', views.user_profile, name='user_profile'),
 ]
